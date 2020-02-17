4d9a0d94b05c952c3a35842b764e5cfc14cdec45
surdtech <surdtech@localhost.localdomain>
2020-02-17 06:45:57 -0500

    added imperative vs functional code

---
+++ imperative-vs-functional.js
@@ -0,0 +1,21 @@
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// find the total of the double of even numbers
+// imperative code
+var imprtv = function (list) {
+  var total = 0
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] % 2 === 0) {
+      total += list[i] * 2
+    }
+  }
+
+  return total
+}
+console.log(imprtv(list))
+// declarative code
+var declrtv = function (list) {
+  return list.filter(function (e) { return e % 2 === 0 })
+    .map(function (e) { return e * 2 })
+    .reduce(function (total, e) { return total + e })
+}
+console.log(declrtv(list))
